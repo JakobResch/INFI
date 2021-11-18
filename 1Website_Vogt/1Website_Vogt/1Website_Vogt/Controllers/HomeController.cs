f8c5e27a421a127369c75deb0a03750f5fbbacb7
JakobResch <jakob.resch04@gmail.com>
2021-11-18 09:38:39 +0100

    1Webiste_Vogt

---
+++ 1Website_Vogt/1Website_Vogt/1Website_Vogt/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1Website_Vogt.Models {
+    public class HomeController : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }
+}
