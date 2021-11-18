8f9cc9b9a736b637016bc3b038aab15e2c6f925a
JakobResch <jakob.resch04@gmail.com>
2021-11-18 10:09:42 +0100

    1Website_Vogt

---
+++ 1Website_Vogt/1Website_Vogt/1Website_Vogt/Controllers/Spieler.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _1Website_Vogt.Controllers {
+    public class Spieler : Controller {
+        public IActionResult Index() {
+            return View();
+        }
+    }f
+}ss
